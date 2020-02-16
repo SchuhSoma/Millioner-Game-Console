e8ed88bcc78cef81aef9ae28da351db1633d4935
SchuhSoma <60043511+SchuhSoma@users.noreply.github.com>
2020-02-16 12:26:40 +0100

    Add files via upload

---
+++ Beolvas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsmetlesNelkuliRandom
+{
+    class Beolvas
+    {
+        public int Sorszam;
+        public string Kerdes;
+        public string ALehetoseg;
+        public string BLehetoseg;
+        public string CLehetoseg;
+        public string DLehetoseg;
+        public string Valasz;
+
+        public Beolvas(string sor)
+        {
+            var dbok = sor.Split(';');
+            this.Sorszam = int.Parse(dbok[0]);
+            this.Kerdes = dbok[1];
+            this.ALehetoseg = dbok[2];
+            this.BLehetoseg = dbok[3];
+            this.CLehetoseg = dbok[4];
+            this.DLehetoseg = dbok[5];
+            this.Valasz = dbok[6];
+        }
+    }
+}
---
+++ Nyeremeny.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsmetlesNelkuliRandom
+{
+    class Nyeremeny
+    {
+        public int NyeremenySorszam;
+        public string NyeremenyOsszeg;
+
+        public Nyeremeny(string sor)
+        {
+            var dbok = sor.Split(';');
+            this.NyeremenySorszam = int.Parse(dbok[0]);
+            this.NyeremenyOsszeg = dbok[1];
+
+        }
+    }
+}
